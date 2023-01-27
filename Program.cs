214b29ab1ea7be2531c5518d34708c8b89919c24
juanhernandezdeveloper <jualherac@gmail.com>
2023-01-27 15:29:16 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using CP;
+
+ParkingSpot newSpot = new ParkingSpot(6); // Set the number of spot to assign for parking
+
+CarPark newPark = new CarPark(10); // Set Capacity of the CarPark
+
+Vehicle myCar = new Vehicle("KYD787");
+
+newPark.AddParkingSpot(newSpot);
+
+
+
+Console.WriteLine(myCar.LicenseNumber);
+
+Console.WriteLine(newPark.GetParkingSpots().Count);
+
+Console.WriteLine(newSpot.CarPark.GetParkingSpots().Count);
+
+Console.WriteLine(newPark.Capacity);
+
+Console.WriteLine(newSpot.CarPark == newPark);
+
+Console.WriteLine(newPark.GetParkingSpots().First() == newSpot);
+
+
